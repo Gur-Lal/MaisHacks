225a3c34335674552e461cd2fac021d8169b71cf
Storm <gurlal5231@gmail.com>
2023-09-30 20:39:15 -0400

    Uploading newest version

--- MaisHacks/main.py
+++ MaisHacks/main.py
@@ -8,6 +8,7 @@ import numpy as np
 # Setting the path for Tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+
 def readImage(image):
     # Convert PIL Image to OpenCV format
     img_cv = np.array(image)
@@ -62,21 +63,62 @@ def readImage(image):
     }
     return dictFood
 
-def is_healthy_food(dictFood, weight):
-    if dictFood["Calories"] > 500 and weight > 70:
+plastic_recycling_info = {
+    '1': {
+        'type': 'PETE or PET (Polyethylene Terephthalate)',
+        'info': 'Recyclable: Mostly recycled; used to make bottles, etc.'
+    },
+    '2': {
+        'type': 'HDPE (High-Density Polyethylene)',
+        'info': 'Recyclable: Commonly recycled; used for milk jugs, etc.'
+    },
+    # Add all the other plastic types and their information
+    # ...
+}
+
+
+
+def is_healthy_food(dictFood, weight, age):
+    calories = dictFood["Calories"]
+    fat = dictFood["Fat"]
+    sodium = dictFood["Sodium"]
+    protein = dictFood["Protein"]
+
+    if calories <= 0 or fat < 0 or sodium < 0 or protein < 0:
+        st.warning("Incomplete or incorrect nutrition information.")
         return False
-    return True
+    
+    fat_calories = fat * 9
+    protein_calories = protein * 4
+    
+    fat_percentage = (fat_calories / calories) * 100
+    protein_percentage = (protein_calories / calories) * 100
+    
+    # Age-dependent Sodium thresholds
+    sodium_lower = 1500 if age < 50 else 1300
+    sodium_upper = 2300 if age < 50 else 2000
+    
+    if (20 <= fat_percentage <= 35) and (10 <= protein_percentage <= 35) and (sodium_lower <= sodium <= sodium_upper):
+        return True
+    else:
+        return False
 
 # Streamlit UI
 st.title("Nutrition Information Extractor")
+st.write('---')
 
-# User details input
-st.subheader("Enter Your Details")
-name = st.text_input("Name")
-age = st.number_input("Age", min_value=1, max_value=120)
-weight = st.number_input("Weight (in kg)", min_value=1.0, max_value=200.0)
+# Improved layout with columns
+col1, col2 = st.columns(2)
 
-uploaded_file = st.file_uploader("Choose an image to check its nutrition...", type=["jpg", "jpeg", "png"])
+with col1:
+    st.subheader("Enter Your Details")
+    name = st.text_input("Name")
+    age = st.number_input("Age", min_value=1, max_value=120, value=30, step=1)
+    weight = st.number_input("Weight (in kg)", min_value=1.0, max_value=200.0, value=70.0, step=0.1)
+    
+with col2:
+    st.subheader("Upload Image")
+    uploaded_file = st.file_uploader("Choose an image to check its nutrition...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
     image = Image.open(uploaded_file)
@@ -84,11 +126,15 @@ if uploaded_file:
     st.write("Processing...")
 
     results = readImage(image)
-
+    
     st.write("Here are the extracted nutrition details:")
     st.json(results)
-
-    if is_healthy_food(results, weight):
+    
+    if is_healthy_food(results, weight, age):  # Remember to pass age here
         st.success("This seems like a healthy food choice for you!")
     else:
         st.warning("This might not be the healthiest choice for you. Consider something lighter!")
+    
+st.write('---')
+st.markdown("For detailed nutritional guidelines, please refer to [Canada's Dietary Guidelines](https://www.canada.ca/en/health-canada/services/food-nutrition/healthy-eating/dietary-reference-intakes/tables/reference-values-macronutrients-dietary-reference-intakes-tables-2005.html)")
+
